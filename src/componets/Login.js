33eb6bd3e9dbe3566cf3d0d8a4e4ae439227e263
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-10-13 16:26:07 -0700

    Google login code added

---
+++ src/componets/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = 'myClientID.google.com';
+
+function Login() {
+    const onSuccess = (res) => {
+        console.log('[Login Success] currentUser:', res.profileObj);
+    };
+
+    const onFailure = (res) => {
+        console.log('[Login failed] res:', res);
+    };
+
+
+return (
+    <div>
+        <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_orgin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+        />
+    </div>
+);
+}
+
+export default Login;
