1932b2c3939737931c0c0f0470423a66b09a4c12
Andy <awilliamscomposing@gmail.com>
2021-10-13 21:44:06 -0700

    Troubleshooting index.html yielding error with localhost 5000

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import React, { useEffect } from "react";
 import "./App.css";
-import Login from "./components/Login";
-import Logout from "./components/LogOut";
+// import Login from "./components/Login";
+// import Logout from "./components/LogOut";
 import firebase from "./Firebase/firebase";
 
 function App() {
