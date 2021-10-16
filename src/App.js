cc15029873a82564117dd742b421dbb19a321931
Andy <awilliamscomposing@gmail.com>
2021-10-16 11:56:25 -0700

    Image upload successful

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import './App.css';
 import Login from './componets/Login';
 import Logout from './componets/LogOut';
+import ImageUpload from './componets/ImageUpload/';
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <Logout />
-      
+      {/* <Login />
+      <Logout /> */}
+      <ImageUpload/>
     </div>
   );
 }
