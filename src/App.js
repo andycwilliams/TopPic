66e1d249f782e16581c33dec1b1a2154093aeb35
Andy <awilliamscomposing@gmail.com>
2021-10-16 14:13:15 -0700

    Firebase troubleshoot

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import { render } from "@testing-library/react";
 import "./App.css";
-import Login from "./componets/Login";
-import Logout from "./componets/LogOut";
 import ImageUpload from "./componets/ImageUpload/";
-// import { render } from "@testing-library/react";
 import React, { useEffect } from "react";
 // import Login from "./components/Login";
 // import Logout from "./components/LogOut";
-import firebase from "./Firebase/firebase";
 
 function App() {
-  useEffect(() => {
-    console.log(firebase);
-  }, []);
-
   return (
     <div className="App">
       <ImageUpload />
