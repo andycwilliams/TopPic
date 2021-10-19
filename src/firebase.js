c274d50a93f054d43b51d73b0b9b7c9e49bd28be
Andy <awilliamscomposing@gmail.com>
2021-10-18 19:46:30 -0700

    Consolidating firebase files, deleting default react app files

--- src/firebase.js
+++ src/firebase.js
@@ -1,33 +1,33 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+// import { initializeApp } from "firebase/app";
+// import { getDatabase } from "firebase/database";
 
-// Set the configuration for your app
-// TODO: Replace with your project's config object
-const firebaseConfig = {
-  apiKey: "apiKey",
-  authDomain: "projectId.firebaseapp.com",
-  databaseURL: "https://databaseName.firebaseio.com",
-  storageBucket: "bucket.appspot.com",
-};
+// // Set the configuration for your app
+// // TODO: Replace with your project's config object
+// const firebaseConfig = {
+//   apiKey: "apiKey",
+//   authDomain: "projectId.firebaseapp.com",
+//   databaseURL: "https://databaseName.firebaseio.com",
+//   storageBucket: "bucket.appspot.com",
+// };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-// Get a reference to the database service
-const database = getDatabase(app);
+// // Get a reference to the database service
+// const database = getDatabase(app);
 
-// service firebase.storage {
-//   match /b/{bucket}/o {
-//     match /{allPaths=**} {
-//       allow read, write: if request.auth != null;
-//     }
-//   }
-// }
+// // service firebase.storage {
+// //   match /b/{bucket}/o {
+// //     match /{allPaths=**} {
+// //       allow read, write: if request.auth != null;
+// //     }
+// //   }
+// // }
 
-// rules_version = '2';
-// service cloud.firestore {
-//   match /databases/{database}/documents {
-//     match /{document=**} {
-//       allow read, write: if false;
-//     }
-//   }
-// }
+// // rules_version = '2';
+// // service cloud.firestore {
+// //   match /databases/{database}/documents {
+// //     match /{document=**} {
+// //       allow read, write: if false;
+// //     }
+// //   }
+// // }
