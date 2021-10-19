84ee47c893fbe89bb61b012367a4b23ae71e9458
Kygan <kygandao23@gmail.com>
2021-10-18 19:08:36 -0700

    styled main landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,8 @@ module.exports = {
   darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        gray: colors.blueGray,
-        red: colors.rose
+      backgroundImage: {
+        'photo-collage': "url('https://images.unsplash.com/photo-1533158307587-828f0a76ef46?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80')",
       }
     },
   },
