5df5d3da249b0e317064563bf12dc0f11285ae01
Andy <awilliamscomposing@gmail.com>
2021-10-19 21:24:19 -0700

    Committing changes from group meeting

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import "./App.css";
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
+import React from "react";
 import Login from "./components/Login";
 import ExplorePage from "./components/ExplorePage";
 
-
 function App() {
-
   const handlePageChange = (e) => {
     e.preventDefault();
-    console.log('clicked to change pages')
+    console.log("clicked to change pages");
     //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
-  }
+  };
   return (
     <div>
-    <div className='flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover'>
+      <div className="flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover">
+        <div className="flex flex-col justify-center items-center w-2/4 h-2/6 bg-gray-200 bg-opacity-90">
+          <p className="text-7xl my-6">Top Pic</p>
+          <p className="text-xl">Find Your Inspiration</p>
+        </div>
 
-      <div className='flex flex-col justify-center items-center w-2/4 h-2/6 bg-gray-200 bg-opacity-90'>
-        <p className='text-7xl my-6'>Top Pic</p>
-        <p className='text-xl'>Find Your Inspiration</p>
-      </div>
+        <div className="flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90">
+          <Login />
+        </div>
 
-      <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
-        <Login />
+        <div className="flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90">
+          <a href="/" onClick={handlePageChange}>
+            Continue without Signing In
+          </a>
+        </div>
       </div>
-
-      <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
-        <a href='/' onClick={handlePageChange}>Continue without Signing In</a>
-      </div>
-      
+      {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^*/}
+      <ExplorePage />
     </div>
-    {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^*/}
-    <ExplorePage />
-    </div>
-
   );
 }
 
--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,9 +1,31 @@
 import uploadImage from "firebase";
 
-const ImageUpload = () => {
-  const handleImageUpload = (something) => {
-    console.log(something);
-    //uploadImage()
+// const imageUpload = () => {
+//   const handleImageUpload = (something) => {
+//     console.log(something);
+//     //uploadImage()
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         onChange={handleImageUpload}
+//         type="file"
+//         id="img"
+//         name="img"
+//         accept="image/*"
+//       />
+//     </div>
+//   );
+// };
+
+const imageUpload = () => {
+  const [uploadedImage, setUploadedImage] = useState("");
+
+  const handleImageUpload = async (something) => {
+    const file = something.target.files[0];
+    const url = await uploadToFirebase(file);
+    setUploadedImage(url);
   };
 
   return (
@@ -15,8 +37,9 @@ const ImageUpload = () => {
         name="img"
         accept="image/*"
       />
+      <img src={uploadedImage} alt="No image yet" />
     </div>
   );
 };
 
-export default ImageUpload;
+export default imageUpload;
