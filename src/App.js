8b1ccb7e2bdb0fa356fddf82ce2853bbc7a4e782
gibsona475 <81275092+gibsona475@users.noreply.github.com>
2021-10-20 15:55:06 -0700

    switch cases added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
-import { render } from "@testing-library/react";
+
 import "./App.css";
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import MainLandingPage from "./components/MainLandingPage";
 import ExplorePage from "./components/ExplorePage";
 
 
 function App() {
 
-  const handlePageChange = (e) => {
+  const [currentPage, setCurrentPage] = useState('Main');
+
+  const handlePageChange = (page, e) => {
     e.preventDefault();
-    console.log('clicked to change pages')
+    console.log('clicked to change pages', page)
     //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
+    setCurrentPage(page);
   }
+
+
+  const renderPage = () => {
+    console.log('RENDER APPJS component', currentPage);
+    //Sswitch case 
+    switch (currentPage) {
+      case 'Main':
+        return <MainLandingPage handlePageChange={handlePageChange} currentPage={'Main'} />;
+      case 'Explore':
+        return <ExplorePage />;
+      default:
+        return <MainLandingPage handlePageChange={handlePageChange} currentPage={'Main'} />
+    }
+  }
+
   return (
     <div>
       <div className='flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover'>
-
-      <MainLandingPage />
-
-        <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
-          <a href='/' onClick={handlePageChange}>Continue without Signing In</a>
-        </div>
-
+        {/* By default its render MAin Planding page  */}
+        {renderPage()}
+        {/* hide or  show this button once the user logins in without signin   */}
+        {currentPage === 'Explore' ? <div></div> :
+          <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
+            {/* on click of this button its render the explore page  */}
+            <a href='/' name='Explore' onClick={(e) => handlePageChange('Explore', e)} > Continue without Signing In</a>
+          </div>
+        }
       </div>
-    {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^ THIS SHOULD NOT BE DISPLAYING UNTIL BUTTON IS CLICKED*/}
-    <ExplorePage />
-  </div>
+
+    </div>
   );
 }
 
