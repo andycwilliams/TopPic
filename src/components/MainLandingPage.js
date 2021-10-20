5dfa239e30e4589b471bd2020c8b343863244334
Kygan <kygandao23@gmail.com>
2021-10-19 21:27:12 -0700

    continuing to split pages, added placeholders for example

---
+++ src/components/MainLandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Login from './Login';
+
+const MainLandingPage = () => {
+    return (
+        <div className='flex flex-col justify-center items-center w-2/4 h-2/6 bg-gray-200 bg-opacity-90'>
+            <p className='text-7xl my-6'>Top Pic</p>
+            <p className='text-xl'>Find Your Inspiration</p>
+            <div className='flex justify-center items-center w-2/4 h-1/6 mt-10'>
+                <Login />
+            </div>
+            
+        </div>
+    )
+}
+
+export default MainLandingPage;
