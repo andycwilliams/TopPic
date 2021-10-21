9c50b33cc6396ebe866b43e1cc0b8a7a89f84de9
Andy <awilliamscomposing@gmail.com>
2021-10-20 19:24:43 -0700

    Prioritizing app.js code

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,3 @@
-// // import { render } from "@testing-library/react";
-// import "./App.css";
-// import React, { useEffect } from "react";
-// import MainLandingPage from "./components/MainLandingPage";
-// import ExplorePage from "./components/ExplorePage";
-
-// function App() {
-//   const handlePageChange = (e) => {
-//     e.preventDefault();
-//     console.log("clicked to change pages");
-//     //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
-//   };
-//   return (
-//     <div>
-//       <div className='flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover'>
-//         <button onClick={() => fetch("/api").then(res => console.log(res))}>Click for api</button>
-//       <MainLandingPage />
-
-//         <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
-//           <a href='/' onClick={handlePageChange}>Continue without Signing In</a>
-//         </div>
-
-//       </div>
-//     {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^ THIS SHOULD NOT BE DISPLAYING UNTIL BUTTON IS CLICKED*/}
-//     <ExplorePage />
-//   </div>
-//   );
-// }
-
-// export default App;
-
-// ----------------------------------------------------------------
-
 import "./App.css";
 import React, { useState } from "react";
 import MainLandingPage from "./components/MainLandingPage";
@@ -96,6 +63,41 @@ function App() {
 }
 
 export default App;
+
+// ----------------------------------------------------------------
+
+// // import { render } from "@testing-library/react";
+// import "./App.css";
+// import React, { useEffect } from "react";
+// import MainLandingPage from "./components/MainLandingPage";
+// import ExplorePage from "./components/ExplorePage";
+
+// function App() {
+//   const handlePageChange = (e) => {
+//     e.preventDefault();
+//     console.log("clicked to change pages");
+//     //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
+//   };
+//   return (
+//     <div>
+//       <div className='flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover'>
+//         <button onClick={() => fetch("/api").then(res => console.log(res))}>Click for api</button>
+//       <MainLandingPage />
+
+//         <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
+//           <a href='/' onClick={handlePageChange}>Continue without Signing In</a>
+//         </div>
+
+//       </div>
+//     {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^ THIS SHOULD NOT BE DISPLAYING UNTIL BUTTON IS CLICKED*/}
+//     <ExplorePage />
+//   </div>
+//   );
+// }
+
+// export default App;
+
+// ----------------------------------------------------------------
 
 // // import { render } from "@testing-library/react";
 // import "./App.css";
