6e95cf3a627713e57945d0c616482ffe45537efc
Andy <awilliamscomposing@gmail.com>
2021-10-22 14:53:47 -0700

    Fixing typos

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,13 @@ function App() {
   const handlePageChange = (page, e) => {
     e.preventDefault();
     console.log("clicked to change pages", page);
-    //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
+    // TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
     setCurrentPage(page);
   };
 
   const renderPage = () => {
     console.log("RENDER APPJS component", currentPage);
-    //Sswitch case
+    // Switch case
     switch (currentPage) {
       case "Main":
         return (
@@ -39,20 +39,20 @@ function App() {
   return (
     <div>
       <div className="flex flex-col justify-center items-center bg-photo-collage bg-no-repeat bg-fixed bg-cover">
-        {/* By default its render MAin Planding page  */}
+        {/* By default it renders Main Landing page */}
         {renderPage()}
-        {/* hide or  show this button once the user logins in without signin   */}
+        {/* Hide or  show this button once the user logins in without signin */}
         {currentPage === "Explore" ? (
           <div></div>
         ) : (
           <div className="hover:bg-blue-300 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90">
-            {/* flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90 */}
-            {/* on click of this button its render the explore page  */}
+            {/* Flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90 */}
+            {/* onClick renders the explore page  */}
             <a
               href="/"
               name="Explore"
               onClick={(e) => handlePageChange("Explore", e)}
-              // this makes it so when you click the continue without sign in in you can view the rest of the application
+              // This makes it so when you click the continue without sign in in you can view the rest of the application
             >
               {" "}
               Continue without Signing In
