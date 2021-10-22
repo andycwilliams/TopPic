7c91d8281772918486d783993bfc163bc8d909b8
Sean Russell <emailseanrussell@gmail.com>
2021-10-22 14:23:33 -0700

    adding picture random view

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,4 @@
-// // import { render } from "@testing-library/react";
-// import "./App.css";
-// import React, { useEffect } from "react";
-// import MainLandingPage from "./components/MainLandingPage";
-// import ExplorePage from "./components/ExplorePage";
 
-// function App() {
-//   const handlePageChange = (e) => {
-//     e.preventDefault();
-//     console.log("clicked to change pages");
-//     //TODO: CREATE FUNCTION TO HANDLE PAGE CHANGE... SWITCH CASE? ROUTER-DOM
-//   };
-//   return (
-//     <div>
-//       <div className='flex flex-col justify-center items-center h-screen bg-photo-collage bg-no-repeat bg-fixed bg-cover'>
-//         <button onClick={() => fetch("/api").then(res => console.log(res))}>Click for api</button>
-//       <MainLandingPage />
-
-//         <div className='flex justify-center items-center w-2/4 h-1/6 bg-gray-200 bg-opacity-90'>
-//           <a href='/' onClick={handlePageChange}>Continue without Signing In</a>
-//         </div>
-
-//       </div>
-//     {/*^^ FIX PAGE TO ROUTE TO EXPLORE PAGE ON BUTTON CLICK ^^ THIS SHOULD NOT BE DISPLAYING UNTIL BUTTON IS CLICKED*/}
-//     <ExplorePage />
-//   </div>
-//   );
-// }
-
-// export default App;
-
-// ----------------------------------------------------------------
 
 import "./App.css";
 import React, { useState } from "react";
@@ -84,6 +53,7 @@ function App() {
               href="/"
               name="Explore"
               onClick={(e) => handlePageChange("Explore", e)}
+              // this makes it so when you click the continue without sign in in you can view the rest of the application 
             >
               {" "}
               Continue without Signing In
