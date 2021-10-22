750db078d4369ab661396dd14b373825d08de5f6
Andy <awilliamscomposing@gmail.com>
2021-10-22 16:24:32 -0700

    Transferring API keys to .env file

--- client/src/components/ImageUpload/firebase.js
+++ client/src/components/ImageUpload/firebase.js
@@ -7,13 +7,12 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 const firebaseConfig = {
   // apiKey: "AIzaSyAHlQK32Pp8yODbhThzvC6vaE36exsGUDs",
   apiKey: process.env.DEV_APP_KEY,
-  apiKey: "AIzaSyAHlQK32Pp8yODbhThzvC6vaE36exsGUDs",
-  authDomain: "top-pic-28acd.firebaseapp.com",
-  databaseURL: "https://top-pic-28acd-default-rtdb.firebaseio.com",
-  projectId: "top-pic-28acd",
-  storageBucket: "top-pic-28acd.appspot.com",
-  messagingSenderId: "722258064323",
-  appId: "1:722258064323:web:ab05a106fde4fca9fcc376",
+  authDomain: process.env.DEV_AUTH_DOMAIN,
+  databaseURL: process.env.DEV_DATABASE_URL,
+  projectId: process.env.DEV_PROJECT_ID,
+  storageBucket: process.env.DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.DEV_MESSAGING_SENDER_ID,
+  appId: process.env.DEV_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
